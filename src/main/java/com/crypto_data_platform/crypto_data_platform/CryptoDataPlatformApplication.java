4d949ca0702c1815bfb33ca37ef82054d8160ace
Iñaki Ramos <iramositurria@gmail.com>
2026-03-18 23:20:07 +0100

    feat: initialize Spring Boot project with base structure

---
+++ src/main/java/com/crypto_data_platform/crypto_data_platform/CryptoDataPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.crypto_data_platform.crypto_data_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoDataPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoDataPlatformApplication.class, args);
+	}
+
+}
