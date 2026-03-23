7617c98c83854e7a0d1100d5c7aeb962a05f3986
Iñaki Ramos <iramositurria@gmail.com>
2026-03-23 23:46:41 +0100

    feat: add scheduler to run crypto ingestion periodically

---
+++ src/main/java/com/crypto_data_platform/crypto_data_platform/scheduler/CryptoScheduler.java
@@ -0,0 +1,24 @@
+package com.crypto_data_platform.crypto_data_platform.scheduler;
+
+import com.crypto_data_platform.crypto_data_platform.service.CryptoService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+// AUTOMATIZAR EL DATA PIPELINE
+@Component
+public class CryptoScheduler {
+
+    private final CryptoService service;
+
+    public CryptoScheduler(CryptoService service) {
+        this.service = service;
+    }
+
+    @Scheduled(fixedRate = 60000) // cada 60 segundos
+    public void runCryptoPipeline() {
+
+        System.out.println("Starting scheduled crypto ingestion...");
+        service.fetchAndSaveCryptoData();
+        System.out.println("Finished scheduled crypto ingestion");
+    }
+}
