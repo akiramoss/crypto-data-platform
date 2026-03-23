40d7b56fb51fff5073d33f29cbd918c8374ee2e5
Iñaki Ramos <iramositurria@gmail.com>
2026-03-23 23:47:59 +0100

    refactor: remove CommandLineRunner in favor of scheduled execution

--- src/main/java/com/crypto_data_platform/crypto_data_platform/CryptoDataPlatformApplication.java
+++ src/main/java/com/crypto_data_platform/crypto_data_platform/CryptoDataPlatformApplication.java
@@ -1,13 +1,9 @@
 package com.crypto_data_platform.crypto_data_platform;
 
 import org.springframework.scheduling.annotation.EnableScheduling;
-import com.crypto_data_platform.crypto_data_platform.domain.CryptoPrice;
-import com.crypto_data_platform.crypto_data_platform.repository.CryptoRepository;
-import com.crypto_data_platform.crypto_data_platform.service.CryptoService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @EnableScheduling // Ejecutar tareas automáticas
 @SpringBootApplication
@@ -15,41 +11,5 @@ public class CryptoDataPlatformApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CryptoDataPlatformApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner testDB(CryptoRepository repo) {
-        return args -> {
-            CryptoPrice btc = new CryptoPrice();
-            btc.setSymbol("BTC");
-            btc.setPrice(65000.0);
-            btc.setMarketCap(1000000.0);
-            btc.setVolume(50000.0);
-            btc.setTimestamp(System.currentTimeMillis());
-
-            repo.save(btc);
-
-            System.out.println("DATA INSERTED");
-        };
-    }
-
-    /*
-    @Bean
-    CommandLineRunner testAPI(CryptoApiClient client) {
-        return args -> {
-            var data = client.fetchCryptoData();
-
-            System.out.println("DATA FROM API:");
-            System.out.println(data[0].getSymbol());
-        };
-    }
-     */
-
-    @Bean
-    CommandLineRunner run(CryptoService service) {
-        return args -> {
-            service.fetchAndSaveCryptoData();
-            System.out.println("CRYPTO DATA SAVED");
-        };
     }
 }
