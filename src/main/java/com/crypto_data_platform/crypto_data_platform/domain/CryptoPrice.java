8b8b6a7526c6bdb90aa1ab4b0c232551abf16f58
Iñaki Ramos <iramositurria@gmail.com>
2026-03-26 21:58:52 +0100

    refactor: replace timestamp with LocalDateTime for better data handling

--- src/main/java/com/crypto_data_platform/crypto_data_platform/domain/CryptoPrice.java
+++ src/main/java/com/crypto_data_platform/crypto_data_platform/domain/CryptoPrice.java
@@ -3,6 +3,8 @@ package com.crypto_data_platform.crypto_data_platform.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -14,8 +16,14 @@ public class CryptoPrice {
     private Long id;
 
     private String symbol;
+
     private Double price;
+
     private Double marketCap;
+
     private Double volume;
-    private Long timestamp;
+
+    // Guardar tiempo real legible
+    // HIBERNATE --> DATETIME
+    private LocalDateTime timeStamp;
 }
