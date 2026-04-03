d480d5bf61d397413cf33ac62d2d9ce77dbaa23c
Iñaki Ramos <iramositurria@gmail.com>
2026-04-03 20:46:51 +0200

    refactor(dto): remove Lombok and implement manual getters/setters for CryptoApiResponse

--- src/main/java/com/crypto_data_platform/crypto_data_platform/dto/CryptoApiResponse.java
+++ src/main/java/com/crypto_data_platform/crypto_data_platform/dto/CryptoApiResponse.java
@@ -6,16 +6,66 @@ import lombok.Data;
 public class CryptoApiResponse {
 
     private String id;
-
     private String symbol;
-
     private String name;
-
     private Double current_price;
-
     private Double market_cap;
-
     private Double total_volume;
-
     private String last_updated;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getCurrent_price() {
+        return current_price;
+    }
+
+    public void setCurrent_price(Double current_price) {
+        this.current_price = current_price;
+    }
+
+    public Double getMarket_cap() {
+        return market_cap;
+    }
+
+    public void setMarket_cap(Double market_cap) {
+        this.market_cap = market_cap;
+    }
+
+    public Double getTotal_volume() {
+        return total_volume;
+    }
+
+    public void setTotal_volume(Double total_volume) {
+        this.total_volume = total_volume;
+    }
+
+    public String getLast_updated() {
+        return last_updated;
+    }
+
+    public void setLast_updated(String last_updated) {
+        this.last_updated = last_updated;
+    }
 }
